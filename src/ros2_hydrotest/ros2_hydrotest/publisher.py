09f24a56c9b849629d7234d0bb8ca879c7a958c0
Egor <gu1307303@gmail.com>
2026-03-18 17:58:37 +0300

    fix publisher node

--- src/ros2_hydrotest/ros2_hydrotest/publisher.py
+++ src/ros2_hydrotest/ros2_hydrotest/publisher.py
@@ -5,23 +5,22 @@ from std_msgs.msg import String
 
 class Publisher(Node):
     def __init__(self):
-        super().__init__("publisher")
-        self.publisher_=self.create_publisher(String,"hydrotest",10)
-        period=0,5
+        super().__init__('publisher')
+        self.publisher_=self.create_publisher(String,'hydrotest',10)
+        period=0.5
         self.timer=self.create_timer(period, self.timer_callback)
         self.counter=0
     def timer_callback(self):
-        msg=String
+        msg=String()
         msg.data="Message N %d" % self.counter
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.counter+=1
 def main(args=None):
     rclpy.init(args=args)
-    publisher=Publisher
-    rclpy.spin(publisher)
-    publisher.destroy_node
-    rclpy.shutdown
+    publisher=Publisher()
+    while rclpy.ok():
+        rclpy.spin_once(publisher, timeout_sec=0.1)
 main()
 
     
