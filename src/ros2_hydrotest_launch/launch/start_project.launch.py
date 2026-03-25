9d6bb308b6620a9690ea5875fc793d1b49eb66eb
Egor <gu1307303@gmail.com>
2026-03-25 11:16:39 +0300

    add launch package

---
+++ src/ros2_hydrotest_launch/launch/start_project.launch.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    word_arg = LaunchConfiguration('word') 
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'word',
+            default_value='HYDRONAUTICS',
+            description='String for client node'
+        ),
+
+        Node(
+            package='ros2_hydrotest',
+            executable='talker',
+            name='publisher',
+            output='screen',
+            prefix='gnome-terminal --'
+        ),
+
+        Node(
+            package='ros2_hydrotest',
+            executable='listener',
+            name='subscriber',
+            output='screen',
+            prefix='gnome-terminal --'
+        ),
+
+        Node(
+            package='ros2_hydrotest_service',
+            executable='service',
+            name='service',
+            output='screen',
+            prefix='gnome-terminal --'
+        ),
+
+        Node(
+            package='ros2_hydrotest_service',
+            executable='client',
+            name='client',
+            output='screen',
+            arguments=[word_arg]
+        ),
+    ])
